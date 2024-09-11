8e3c4a6426f71fb15ce38a43d54e1bc5529a5fb2
Vincent <vincent.devilers@gmail.com>
2024-09-11 22:28:28 +0200

    wip

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/VincentDevi/JobsTrack/internal/view" // Correct import path
+	"github.com/a-h/templ"
 )
 
 func main() {
-	fmt.Println("Server running on port 8080 ")
 
+	http.Handle("/", templ.Handler(view.Hello()))
+
+	fmt.Println("Server running on port 8080 ")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
